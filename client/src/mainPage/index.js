abcd1285e261b0fc2d46a54b5b88b65bb65f28e5
Kendrick Gardner <kbgar24@gmail.com>
2018-01-23 16:21:14 -0600

    refactor

---
+++ client/src/mainPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render = () => (
+    <div>
+      <h1>Main Page</h1>
+      <p>Hello World :)</p>
+    </div>
+  );
+};
