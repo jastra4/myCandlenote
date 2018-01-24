55361463f242bfec5cbc7e3f48baf15c581fb720
Jongerr <jontmichie@gmail.com>
2018-01-24 17:12:05 -0600

    Add actions for notes

---
+++ client/src/actions/notesActions.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+export const addNote = noteInfo => ({
+  type: 'ADD_NOTE',
+  payload: noteInfo,
+});
+
+export const setNotes = notes => ({
+  type: 'SET_NOTES',
+  payload: notes,
+});
+
+export const getNotes = userId => (
+  dispatch => (
+    axios.get(`/notes/${userId}`)
+      .then((res) => {
+        console.log('Getting note:', res.data);
+        dispatch(setNotes(res.data));
+      }, err => console.log(err))
+  )
+);
+
+export const setCurrentnote = noteId => ({
+  type: 'SET_CURRENT_NOTES',
+  payload: { id: noteId },
+});
+
+export const deleteNote = noteId => (
+  dispatch => (
+    axios.post('/deleteNote/', noteId)
+      .then((res) => {
+        console.log('Delete note res:', res.body);
+        dispatch({
+          type: 'DELETE_NOTE',
+          payload: { id: noteId },
+        });
+      })
+  )
+);
