42cb3ad4f5fc65038f08be9798c93a521b446270
Jongerr <jontmichie@gmail.com>
2018-01-24 13:48:45 -0600

    Create action for flashcards

--- client/src/actions/flashcardsActions.js
+++ client/src/actions/flashcardsActions.js
@@ -6,5 +6,32 @@ export const addFlashcard = (cardInfo) => ({
 });
 
 export const setFlashcards = (cards) => ({
-  
+  type: 'SET_FLASH_CARDS',
+  payload: cards
 });
+
+export const getFlashcards = (deckId) => {
+  return (dispatch) => {
+    return axios.get('/flashCards/' + deckId)
+      .then(res => {
+        dispatch(setFlashcards(res.data));
+      }, err => console.log(err));
+  }
+}
+
+export const setCurrentFlashcard = (cardId) => ({
+  type: 'SET_CURRENT_FLASHCARD',
+  payload: {id: cardId}
+});
+
+export const deleteFlashcard = (cardId) => {
+  return (dispatch) => {
+    return axios.post('/deleteCard', cardId)
+      .then(res => {
+        dispatch({
+          type: 'DELETE_FLASHCARD',
+          payload: {id: cardId}
+        })
+      });
+  }
+}
