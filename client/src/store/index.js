f3225c2f10bf7ce3454e131e96ab272bd0e7ef33
Jongerr <jontmichie@gmail.com>
2018-01-24 09:16:21 -0600

    Create demo store for examples

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,3 +1,77 @@
 import { createStore } from 'redux';
 
 import reducers from '../reducers';
+
+
+/*
+
+  demoState = {
+    user: {
+      username: 'Jon',
+      oathId: 'whatever'
+    },
+    resources: {
+      notes: {
+        3: {
+          subject: 'English',
+          heading: 'Predicate Nominatives',
+          body: 'Lorem Ipsum...',
+        },
+        5: {
+          subject: 'Comp Sci',
+          heading: 'Higher order functions',
+          body: 'Lorem Ipsum...',
+        }
+      },
+      decks: {
+        2: {
+          subject: 'Physics',
+          title: 'Kinematics',
+          flashcards: [
+            {
+              front: 'Sciency Question',
+              back: 'Sciency Answer',
+            },
+            {
+              front: 'Another Sciency Question',
+              back: 'Another Sciency Answer',
+            }
+          ],
+        },
+        10: {
+          subject: 'Math',
+          title: 'Derivatives',
+          flashcards: [
+            {
+              front: 'Math Question',
+              back: 'Math Answer',
+            },
+            {
+              front: 'Another Math Question',
+              back: 'Another Math Answer',
+            },
+          ],
+        }
+      },
+      videos: [
+        {
+          title: 'Video Title',
+          url: 'https://www.youtube.com/somethingfromyoutube,
+          starred: true,
+        }
+      ],
+    },
+    privateMessages: [
+      {
+        sendFrom: 'Some Name',
+        timeSent: 2131244325,
+        messageBody: 'Lorem Ipsum...',
+        hasBeenRead: false
+      }
+    ],
+    selectedNote: 3,
+    selectedFlashcard: 2,
+    selectedVideo: 1,
+  }
+
+*/
