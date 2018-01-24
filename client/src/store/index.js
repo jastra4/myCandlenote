98f91a5e77ddec6a7cbba66233256d8577fefb46
Jongerr <jontmichie@gmail.com>
2018-01-24 11:19:22 -0600

    Change store to use standard form of object by id

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -11,76 +11,95 @@ import reducers from '../reducers';
       userId: 5,
       oathId: 'whatever',
     },
-    resources: {
-      notes: {
-        noteList: [
-          3: {
-            subject: 'English',
-            heading: 'Predicate Nominatives',
-            body: 'Lorem Ipsum...',
-          },
-          5: {
-            subject: 'Comp Sci',
-            heading: 'Higher order functions',
-            body: 'Lorem Ipsum...',
-          }
-        ],
-        selectedNote: 5,
+    notes: {
+      byId: {
+        3: {
+          id: 3,
+          subject: 'English',
+          heading: 'Predicate Nominatives',
+          body: 'Lorem Ipsum...',
+        },
+        5: {
+          id: 5,
+          subject: 'Comp Sci',
+          heading: 'Higher order functions',
+          body: 'Lorem Ipsum...',
+        }
       },
-      decks: {
-        deckList: [
-          7: {
-            subject: 'Physics',
-            title: 'Kinematics',
-            cards: [
-              {
-                front: 'Sciency Question',
-                back: 'Sciency Answer',
-              },
-              {
-                front: 'Another Sciency Question',
-                back: 'Another Sciency Answer',
-              }
-            ],
-          },
-          10: {
-            subject: 'Math',
-            title: 'Derivatives',
-            cards: [
-              {
-                front: 'Math Question',
-                back: 'Math Answer',
-              },
-              {
-                front: 'Another Math Question',
-                back: 'Another Math Answer',
-              },
-            ],
-          },
-        ],
-        selectedDeck: 10,
+      selectedNote: 5,
+      allIds: [3, 5],
+    },
+    decks: {
+      byId: {
+        7: {
+          id: 7,
+          subject: 'Physics',
+          title: 'Kinematics',
+        },
+        10: {
+          id: 10,
+          subject: 'Math',
+          title: 'Derivatives',
+        },
       },
-      videos: {
-        videoList: [
-          10: {
-            title: 'Video Title',
-            url: 'https://www.youtube.com/somethingfromyoutube,
-            starred: true,
-          }
-        ],
-        selectedVideo: 10,
+      selectedDeck: 10,
+      allIds: [7, 10],
+    },
+    flashcards: {
+      byId: {
+        11: {
+          id: 11,
+          deckId: 7,
+          front: 'Sciency Question',
+          back: 'Sciency Answer',
+        },
+        13: {
+          id: 13,
+          deckId: 7,
+          front: 'Another Sciency Question',
+          back: 'Another Sciency Answer',
+        },
+        17: {
+          id: 17,
+          deckId: 10,
+          front: 'Math Question',
+          back: 'Math Answer',
+        },
+        20: {
+          id: 20,
+          deckId: 10,
+          front: 'Another Math Question',
+          back: 'Another Math Answer',
+        },
       },
+      selectedFlashcard: 17,
+      allIds[11, 13, 17, 20],
+    }
+    videos: {
+      byId: {
+        10: {
+          id: 10,
+          title: 'Video Title',
+          url: 'https://www.youtube.com/somethingfromyoutube,
+          starred: true,
+        }
+      },
+      selectedVideo: 10,
+      allIds: [10],
     },
     privateMessages: {
-      messages: [
+      byId: {
         8: {
+          id: 8,
+          userId: 5,
           sendFrom: 'Some Name',
           timeSent: 2131244325,
           messageBody: 'Lorem Ipsum...',
           hasBeenRead: false,
         }
-      ],
+      },
       selectedMessage: 8,
+      allIds: [8],
     },
   }
 
