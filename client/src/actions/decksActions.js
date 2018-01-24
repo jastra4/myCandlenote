9e9dcd3d634694f1078f5be852b7080e3a836d7e
Jongerr <jontmichie@gmail.com>
2018-01-24 17:07:44 -0600

    Add actions for videos and decks

---
+++ client/src/actions/decksActions.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+export const addDeck = deckInfo => ({
+  type: 'ADD_DECK',
+  payload: deckInfo,
+});
+
+export const setDecks = decks => ({
+  type: 'SET_DECKS',
+  payload: decks,
+});
+
+export const getDecks = deckId => (
+  dispatch => (
+    axios.get(`/decks/${deckId}`)
+      .then(
+        res => dispatch(setDecks(res.data)),
+        err => console.log(err),
+      )
+  )
+);
+
+export const setCurrentDeck = deckId => ({
+  type: 'SET_CURRENT_DECK',
+  payload: { id: deckId },
+});
+
+export const deleteDeck = deckId => (
+  dispatch => (
+    axios.post('/deleteDeck/', deckId)
+      .then((res) => {
+        console.log('Delte deck response:', res.data);
+        dispatch({
+          type: 'DELETE_DECK',
+          payload: { id: deckId },
+        });
+      })
+  )
+);
