41b39a06d6798cd2c08fa7c52ac06f8cb03335ec
Jongerr <jontmichie@gmail.com>
2018-01-26 12:04:59 -0600

    Add header elemnt to deck

--- client/src/decksPage/DeckView.js
+++ client/src/decksPage/DeckView.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Card, Segment, Icon } from 'semantic-ui-react';
+import { Card, Segment, Icon, Header } from 'semantic-ui-react';
+
+const deckStyle = {
+  fontSize: '130%',
+  overflowWrap: 'break-word',
+};
 
 const DeckView = (props) => {
   const decks = Object.keys(props.decksById).map(key => props.decksById[key]);
@@ -8,9 +13,16 @@ const DeckView = (props) => {
 
   return (
     <div>
-      <p>You have {decks.length} decks!</p>
       <Segment>
-        <Card.Group itemsPerRow={4}>
+        <Header as='h2' icon textAlign="center">
+          <Icon name='clone' />
+          Decks
+          <Header.Subheader>
+            Select a deck to view.
+          </Header.Subheader>
+        </Header>
+        <br></br>
+        <Card.Group itemsPerRow={3}>
           {decks.map(deck => (
             <Card
               key={deck.id}
@@ -19,18 +31,22 @@ const DeckView = (props) => {
               <Card.Content>
                 <Icon floated="right" name="remove" onClick={() => props.deleteDeck(deck.id)} />
                 <Segment
+                  color="brown"
+                  textAlign="center"
+                  padded
                   stacked
                   onClick={() => {
                     props.setCurrentDeck(deck.id);
                     props.history.push('/flashcards');
                   }}
                 >
-                  <p>
-                    Subject: {deck.subject}
-                    <br></br>
-                    Title: {deck.title}
+                  <p style={deckStyle}>
+                    {deck.title}
                   </p>
                 </Segment>
+                <Card.Meta>
+                  Subject: {deck.subject}
+                </Card.Meta>
               </Card.Content>
             </Card>))}
         </Card.Group>
