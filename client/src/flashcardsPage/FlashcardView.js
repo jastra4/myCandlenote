8e88df34fc9b3249a90c771856dd79a7e158d114
Jongerr <jontmichie@gmail.com>
2018-01-26 15:03:35 -0600

    Add semantic ui elements to falshcards view

--- client/src/flashcardsPage/FlashcardView.js
+++ client/src/flashcardsPage/FlashcardView.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactCardFlip from 'react-card-flip';
+import { Segment, Button, Header, Icon } from 'semantic-ui-react';
+
+const styles = {
+  containerDiv: {
+    padding: '2vw',
+  },
+  cardText: {
+    fontSize: '5em',
+  },
+  cardButtons: {
+    marginTop: '10vw',
+  },
+  paddingDiv: {
+    height: '160px',
+  },
+  mainDiv: {
+    padding: '1vw 4vw',
+    height: '35vw',
+  },
+};
 
 class FlashcardView extends React.Component {
   constructor(props) {
     super(props);
 
+    this.defaultCard = {
+      front: 'Sorry, there are no cards in this deck yet.',
+      back: 'Once you add some cards, this one will disappear.',
+    };
+
     this.state = {
       index: 0,
-      cards: this.props.cards,
+      cards: this.props.cards.length ? this.props.cards : [this.defaultCard],
       flipped: false,
     };
   }
@@ -25,21 +50,44 @@ class FlashcardView extends React.Component {
   }
 
   render() {
+    const { cards, index } = this.state;
+
     return (
-      <div>
-        <div>
-          <button type="button" onClick={() => this.changeIndex(-1)}>Prev Card</button>
-          <button type="button" onClick={() => this.changeIndex(1)}>Next Card</button>
-        </div>
-        <div onClick={() => this.flipCard()}>
-          <ReactCardFlip isFlipped={this.state.flipped} infinite={true} onClick={() => {
-            console.log(this.state);
-            this.flipCard();
-          }}>
-            <h1 key="front">{this.state.cards[this.state.index % this.state.cards.length].front}</h1>
-            <h1 key="back">{this.state.cards[this.state.index % this.state.cards.length].back}</h1>
-          </ReactCardFlip>
-        </div>
+      <div style={styles.containerDiv}>
+        <Segment size="massive">
+          <div style={styles.mainDiv}>
+            <div onClick={() => this.flipCard()}>
+              <ReactCardFlip isFlipped={this.state.flipped} infinite={true} onClick={() => {
+                console.log(this.state);
+                this.flipCard();
+              }}>
+                <Segment key="front">
+                  <p style={styles.cardText}>{cards[index % cards.length].front}</p>
+                </Segment>
+                <Segment key="back">
+                  <p style={styles.cardText}>{cards[index % cards.length].back}</p>
+                </Segment>
+              </ReactCardFlip>
+            </div>
+
+            <div style={styles.paddingDiv}></div>
+
+            <div style={styles.cardButtons}>
+              <Button animated onClick={() => this.changeIndex(1)} size="big" attach="bottom">
+                <Button.Content visible>Prev Card</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='left arrow' />
+                </Button.Content>
+              </Button>
+              <Button animated onClick={() => this.changeIndex(-1)} size="big" attach="bottom">
+                <Button.Content visible>Next Card</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='right arrow' />
+                </Button.Content>
+              </Button>
+            </div>
+          </div>
+        </Segment>
       </div>
     );
   }
