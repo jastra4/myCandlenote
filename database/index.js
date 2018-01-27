dcdc660ef4ff68c3a6cbef644e253d629029696c
Jongerr <jontmichie@gmail.com>
2018-01-27 12:37:26 -0600

    Setup basic db models

--- database/index.js
+++ database/index.js
@@ -1,27 +1,36 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGOLAB_RED_URI);
-// const db = mongoose.connection;
+export const db = mongoose.connection;
 
-// const decks = mongoose.Schema({
-//   subject: String,
-//   title: String,
-// });
+export const decksSchema = mongoose.Schema({
+  subject: String,
+  title: String,
+  userId: String,
+});
 
-// const flashcards = mongoose.Schema({
-//   deckId: Number,
-//   front: String,
-//   back: String,
-// });
+export const decks = mongoose.model('decks', decksSchema);
 
-// const notes = mongoose.Schema({
-//   subject: String,
-//   heading: String,
-//   body: String,
-// });
+export const flashcardsSchema = mongoose.Schema({
+  deckId: Number,
+  front: String,
+  back: String,
+});
 
-// const videos = mongoose.Schema({
-//   title: String,
-//   url: String,
-//   starred: Boolean,
-// });
+export const flashcards = mongoose.model('flashcards', flashcardsSchema);
+
+export const notesSchema = mongoose.Schema({
+  subject: String,
+  heading: String,
+  body: String,
+});
+
+export const notes = mongoose.model('notes', notesSchema);
+
+export const videosSchema = mongoose.Schema({
+  title: String,
+  url: String,
+  starred: Boolean,
+});
+
+export const videos = mongoose.model('videos', videosSchema);
