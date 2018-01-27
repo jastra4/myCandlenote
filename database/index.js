6ab07be24a9eccf63e48eebfa1a8abec3b6c5ab9
Brooks Naylor <brooksnaylor@gmail.com>
2018-01-27 11:45:05 -0600

    Closes #1 Fixing linter errors

--- database/index.js
+++ database/index.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_RED_URI);
 // const db = mongoose.connection;
 
-const decks = mongoose.Schema({
-  subject: String,
-  title: String,
-});
+// const decks = mongoose.Schema({
+//   subject: String,
+//   title: String,
+// });
 
-const flashcards = mongoose.Schema({
-  deckId: Number,
-  front: String,
-  back: String,
-});
+// const flashcards = mongoose.Schema({
+//   deckId: Number,
+//   front: String,
+//   back: String,
+// });
 
-const notes = mongoose.Schema({
-  subject: String,
-  heading: String,
-  body: String,
-});
+// const notes = mongoose.Schema({
+//   subject: String,
+//   heading: String,
+//   body: String,
+// });
 
-const videos = mongoose.Schema({
-  title: String,
-  url: String,
-  starred: Boolean,
-});
+// const videos = mongoose.Schema({
+//   title: String,
+//   url: String,
+//   starred: Boolean,
+// });
--- server/config/keys.js
+++ server/config/keys.js
@@ -7,10 +7,6 @@ module.exports = {
     appID: '124123284954960',
     appSecret: '7994d00d4e3b07994a5de7e7aa055eeb',
   },
-  mongodb: {
-    dbURI: 'mongodb://root:root@ds045604.mlab.com:45604/candlenote',
-  },
-  session: {
-    cookieKey: 'candlenotesecretcookies',
-  }
+  mongodb: { dbURI: 'mongodb://root:root@ds045604.mlab.com:45604/candlenote' },
+  session: { cookieKey: 'candlenotesecretcookies' },
 };
