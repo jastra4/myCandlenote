ead703dcdc3aba1a9cc0cbf4fa2a3c3e14d0046d
Kendrick Gardner <kbgar24@gmail.com>
2018-01-27 17:32:09 -0600

    Basic notebox complete

--- client/src/noteBox/index.js
+++ client/src/noteBox/index.js
@@ -1,31 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Image } from 'semantic-ui-react';
+import Note from './note';
 
-const style = {
-  float: 'left',
-  marginLeft: '30px'
-};
+const titles = [
+  'My First Note',
+  'My Second Note',
+  'My Third Note'
+];
 
-export default class NotePage extends React.Component {
-  
-  constructor(props) {
-    super(props);
-  }
-
-  componentWillMount() {
-  }
-
-  render = () => (
-    <div>
-      <div style={{ width: '150px', height: '193px'  }}>
-        <img src='/assets/notepad.png' style={{ width: '100%', height: '100%' }} />
-        <h2 style={ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }>This is my text</h2>
-      </div>
-
-      <Image src='/assets/notepad.png' size='small' style={style}/>
-      <Image src='/assets/notepad.png' size='small' style={style}/>
-      
-    </div>
-  );
-}
-// {/* <div style={{ backgroundImage: 'url("/assets/notepad.png")'}}>This is my text</div> */}
+export default () => (
+  <div>
+    { titles.map((title, i) => <Note title={ title } key={ i }/>)  }
+  </div>
+);
