5a715c3d75c37a0507f47e4267753f100dd5e68c
Kendrick Gardner <kbgar24@gmail.com>
2018-01-27 22:40:17 -0600

    IntelliSearch in progress

---
+++ client/src/notePad/intelliSearch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class IntelliSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render = () => (
+    <div>
+      IntelliSearch!!!!
+  
+    </div>
+  );
+}
--- client/src/notepad/index.js
+++ client/src/notepad/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import NotePad from './notePad';
 import NoteTitle from './NoteTitle';
+import FileMenu from './fileMenu';
+import IntelliSearch from './intelliSearch';
 
 export default class NotePage extends React.Component {
   constructor(props) {
@@ -15,38 +17,16 @@ export default class NotePage extends React.Component {
 
   render = () => (
     <div>
-      <Button animated='fade'>
-        <Button.Content hidden>New</Button.Content>
-        <Button.Content visible>
-          <Icon name='file' />
-        </Button.Content>
-      </Button>
-      <Button animated='fade'>
-        <Button.Content hidden>Email</Button.Content>
-        <Button.Content visible>
-          <Icon name='mail' />
-        </Button.Content>
-      </Button>
-      <Button animated='fade'>
-        <Button.Content hidden>Print</Button.Content>
-        <Button.Content visible>
-          <Icon name='print' />
-        </Button.Content>
-      </Button>
-      <Button animated='fade'>
-        <Button.Content hidden>PDF</Button.Content>
-        <Button.Content visible>
-          <Icon name='file pdf outline' />
-        </Button.Content>
-      </Button>
-      <Button animated='fade'>
-        <Button.Content hidden>Delete</Button.Content>
-        <Button.Content visible>
-          <Icon name='trash' />
-        </Button.Content>
-      </Button>
-      <NoteTitle />
-      <NotePad { ...this.props } />
+      <Grid >
+        <Grid.Column width={12}>
+          <FileMenu />
+          <NoteTitle />
+          <NotePad { ...this.props } />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <IntelliSearch />
+        </Grid.Column>
+      </Grid >
   </div>
   );
 }
