82bcf3ed3a9449adb972f2f65682ec909e384a18
Jongerr <jontmichie@gmail.com>
2018-01-30 12:34:06 -0600

    Create actions and reducer for intellisearch

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -6,6 +6,7 @@ import notesReducer from './notesReducers';
 import usersReducer from './usersReducers';
 import messagesReducer from './messagesReducers';
 import videosReducer from './videosReducers';
+import intellisearchReducer from './intellisearchReducers';
 
 const reducers = combineReducers({
   decks: decksReducer,
@@ -14,6 +15,7 @@ const reducers = combineReducers({
   user: usersReducer,
   messages: messagesReducer,
   videos: videosReducer,
+  intellisearch: intellisearchReducer,
 });
 
 export default reducers;
