ccfc389a72bd71f7897bc5ffa1e0ae8f721484bd
Jongerr <jontmichie@gmail.com>
2018-01-31 10:47:23 -0600

    Render wiki results to sidebar

--- client/src/notePad/intelliSearch/WikiSearchResultsEntry.js
+++ client/src/notePad/intelliSearch/WikiSearchResultsEntry.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default props => (
   <li>
-    <a href={props.result.formattedUrl} target="_blank">{props.result.htmlTitle}</a>
-    <p>{props.result.htmlSnippet}</p>
+    <a href={props.result.url} target="_blank">{props.result.title}</a>
+    <p>{props.result.description}</p>
   </li>
 );
