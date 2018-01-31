1b7d534edb2159ae3f1367419c234d82220fabe7
Jongerr <jontmichie@gmail.com>
2018-01-30 19:24:08 -0600

    Create component for listing google search resutls

--- client/src/notePad/intelliSearch/index.js
+++ client/src/notePad/intelliSearch/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import axios from 'axios';
 import YouTubeList from './youtubeList';
+import SearchResultsEntry from './SearchResultEntry';
 // import axios from 'axios';
 
 
 export default class IntelliSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { searchResults: [] };
   }
 
   componentWillReceiveProps(newProps) {
     console.log('Props before:', this.props, newProps);
-    const searchTerms = newProps.meaning.trim().split('').join('+');
+    const searchTerms = newProps.meaning.trim().split(' ').join('+');
     console.log('Search Terms:', searchTerms);
     axios.post('/api/suggestedResources', { searchTerms })
-      .then(res => console.log('Response data:', res.data))
+      .then((res) => {
+        console.log('Response data:', res.data);
+        this.setState({ searchResults: res.data.items });
+      })
       .catch(err => console.log('ERR:', err));
   }
 
@@ -25,7 +29,13 @@ export default class IntelliSearch extends React.Component {
 
   render = () => (
     <div>
+      <p>Hello</p>
       {/* <YouTubeList /> */}
+      <ul>
+        {this.state.searchResults.map(result => (
+          <SearchResultsEntry result={result} />
+        ))}
+      </ul>
     </div>
   );
 }
