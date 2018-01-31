17b6b2256959abba67a0d04aaedeb32f99923de0
Jongerr <jontmichie@gmail.com>
2018-01-30 19:39:58 -0600

    Create a container component for google results entries

--- client/src/notePad/intelliSearch/index.js
+++ client/src/notePad/intelliSearch/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import axios from 'axios';
 import YouTubeList from './youtubeList';
-import SearchResultsEntry from './SearchResultEntry';
+import SearchResults from './SearchResults';
 // import axios from 'axios';
 
 
 export default class IntelliSearch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searchResults: [] };
+    this.state = {
+      searchResults: [],
+      videos: [],
+    };
   }
 
   componentWillReceiveProps(newProps) {
     console.log('Props before:', this.props, newProps);
     const searchTerms = newProps.meaning.trim().split(' ').join('+');
+    this.grabGoogleSearch(searchTerms);
+  }
+
+  grabGoogleSearch(searchTerms) {
+    console.log('Search Terms:', searchTerms);
+    axios.post('/api/suggestedResources', { searchTerms })
+      .then((res) => {
+        console.log('Response data:', res.data);
+        this.setState({ searchResults: res.data.items });
+      })
+      .catch(err => console.log('ERR:', err));
+  }
+
+  grabYoutubeSearch(searchTerms) {
     console.log('Search Terms:', searchTerms);
     axios.post('/api/suggestedResources', { searchTerms })
       .then((res) => {
@@ -31,11 +48,7 @@ export default class IntelliSearch extends React.Component {
     <div>
       <p>Hello</p>
       {/* <YouTubeList /> */}
-      <ul>
-        {this.state.searchResults.map(result => (
-          <SearchResultsEntry result={result} />
-        ))}
-      </ul>
+      <SearchResults results={this.state.searchResults} />
     </div>
   );
 }
