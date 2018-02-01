3bc489df09634c48f336963262a20cae9fa76b41
Joseph Strandmo <josephstrandmo@gmail.com>
2018-02-01 16:57:13 -0600

    Fix linting issue

--- client/src/reducers/socketReducers.js
+++ client/src/reducers/socketReducers.js
@@ -1,9 +1,9 @@
 const socketReducer = (state = false, action) => {
   switch (action.type) {
     case 'ACTIVE_SOCKET':
-      return { 
-      	socket: action.socket,
-      	username: action.username,
+      return {
+        socket: action.socket,
+        username: action.username,
       };
     default:
       return state;
