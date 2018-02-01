5b7bf2a47b0addca9c0419befa0a90426550a662
Jongerr <jontmichie@gmail.com>
2018-01-31 19:29:28 -0600

    Switch to semantic ui grids for flashcard page

--- client/src/flashcardsPage/index.js
+++ client/src/flashcardsPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Header, Icon, Card, Divider } from 'semantic-ui-react';
+import { Segment, Header, Icon, Card, Divider, Grid } from 'semantic-ui-react';
 
 import FlashcardCreator from './FlashcardCreator';
 import FlashcardView from './FlashcardView';
@@ -15,15 +15,13 @@ const styles = {
     verticalAlign: 'top',
     width: '28%',
     maxWidth: '130px',
-    marginTop: '20vh',
+    marginTop: '13em',
     // marginRight: '2vw',
   },
   sideBar: {
-    paddingLeft: '2em',
-    marginTop: '40px',
-    display: 'inline-block',
-    width: '25%',
-    verticalAlign: 'top',
+    marginTop: '3.5em',
+    position: 'relative',
+    right: '-25px',
   },
   mainSegment: {
     display: 'inline-block',
@@ -33,43 +31,47 @@ const styles = {
 };
 
 const FlashcardPage = props => (
-  <div style={styles.containerDiv}>
-    <Segment size="massive">
-      <div style={styles.mainSegment}>
-        <div style={styles.headerContainer}>
-          <Header as='h2' icon>
-            <Icon name='clone' />
-              Deck:
+  <div style={{marginLeft: '20px'}}>
+    {/* <Segment size="massive"> */}
+    <Grid>
+      <Grid.Column width={13}>
+        {/* <div style={styles.mainSegment}> */}
+          <div style={styles.headerContainer}>
+            <Header as='h2' icon>
+              <Icon name='clone' />
+              <h3>Deck:</h3>
               <Header.Subheader>
-              {props.currentDeck.title || 'No Deck Selected'}
-            </Header.Subheader>
-          </Header>
+                {props.currentDeck.title || 'No Deck Selected'}
+              </Header.Subheader>
+            </Header>
+          </div>
+          <FlashcardView
+            cards={props.cards}
+            currentDeck={props.currentDeck}
+          />
+          <Divider />
+          <Header as="h2">Create New Flashcards</Header>
+          <FlashcardCreator
+            currentDeck={props.currentDeck}
+            addFlashcard={props.addFlashcard}
+            createFlashcard={props.createFlashcard}
+            decksById={props.decksById}
+          />
+        {/* </div> */}
+      </Grid.Column>
+      <Grid.Column width={3}>
+        <div style={styles.sideBar}>
+          <Card fluid>
+            <Card.Content>
+              <Card.Header textAlign="center">
+                <h2>Decks</h2>
+                </Card.Header>
+                <DeckListVert />
+            </Card.Content>
+          </Card>
         </div>
-        <FlashcardView
-          cards={props.cards}
-          currentDeck={props.currentDeck}
-        />
-        <Divider />
-        <Header as="h2">Create New Flashcards</Header>
-        <FlashcardCreator
-          currentDeck={props.currentDeck}
-          addFlashcard={props.addFlashcard}
-          createFlashcard={props.createFlashcard}
-          decksById={props.decksById}
-        />
-      </div>
-
-      <div style={styles.sideBar}>
-        <Card>
-          <Card.Content>
-            <Card.Header textAlign="center">
-              Decks
-              </Card.Header>
-              <DeckListVert />
-          </Card.Content>
-        </Card>
-      </div>
-    </Segment>
+      </Grid.Column>
+    </Grid>
   </div>
 );
 
