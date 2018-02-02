2c189913951cb24965d4a069e6d3c36131f91b32
Jongerr <jontmichie@gmail.com>
2018-02-02 09:47:52 -0600

    Show warning message when user does not fill out all required fields

--- client/src/decksPage/DeckCreator.js
+++ client/src/decksPage/DeckCreator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, Segment, Divider, Grid } from 'semantic-ui-react';
+import { Form, Input, Button, Segment, Divider, Grid, Message } from 'semantic-ui-react';
 
 class DeckCreator extends React.Component {
   constructor(props) {
@@ -8,6 +8,7 @@ class DeckCreator extends React.Component {
     this.state = {
       subject: '',
       title: '',
+      improperSubmit: false,
     };
   }
 
@@ -21,18 +22,20 @@ class DeckCreator extends React.Component {
 
   onFormSubmit(e) {
     e.preventDefault();
-    const deckInfo = {
-      ...this.state,
-      userId: this.props.userId,
-    };
-
-    // this.props.addDeck(deckInfo);
-    this.props.createDeck(deckInfo);
-
-    this.setState({
-      subject: '',
-      title: '',
-    });
+    if (!this.state.subject || !this.state.title) {
+      this.setState({ improperSubmit: true });
+    } else {
+      const deckInfo = {
+        ...this.state,
+        userId: this.props.userId,
+      };
+      this.props.createDeck(deckInfo);
+      this.setState({
+        subject: '',
+        title: '',
+        improperSubmit: false,
+      });
+    }
   }
 
   render() {
@@ -49,6 +52,11 @@ class DeckCreator extends React.Component {
                   <Divider />
                   <Form.Field control={Input} value={this.state.title} label='Title' placeholder='Title' onChange={e => this.onTitleChange(e)} />
                   <Form.Field id='form-button-control-public' control={Button} content='Confirm' label='Create Deck' />
+                  {this.state.improperSubmit ?
+                    <Message negative onDismiss={() => this.setState({ improperSubmit: false })}>
+                      <Message.Header>Missing Field</Message.Header>
+                      <p>You must provide both a subject and title</p>
+                    </Message> : ''}
                 </Grid.Column>
                 <Grid.Column width={10}>
                   <div style={{ paddingTop: '1.65em' }}>
