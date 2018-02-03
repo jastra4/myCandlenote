39c63ab0d57990c0cbb11869f4b47248f7b7fc05
Jongerr <jontmichie@gmail.com>
2018-02-03 14:31:11 -0600

    Add google calendar to passport scope

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -15,7 +15,9 @@ router.get('/logout', (req, res) => {
   res.redirect('/auth/login');
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google', passport.authenticate('google', { scope: [
+  'profile',
+  'https://www.googleapis.com/auth/calendar.readonly'] }));
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
   console.log('Logged in user: ', req.user.username);
