9aa763144bbb1001b066ad03e5dd7be561a8e4fe
Jongerr <jontmichie@gmail.com>
2018-02-07 16:19:29 -0600

    Add trasistions to logo and make a square logo

--- client/src/mainPage/index.js
+++ client/src/mainPage/index.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import { Motion, spring } from 'react-motion';
+import { Image } from 'semantic-ui-react';
+
+const fadeAnimationStart = {
+  x: 0,
+  height: 0,
+};
+const fadeAnimationEnd = {
+  x: spring(1, {
+    stiffness: 60,
+    damping: 26,
+  }),
+  height: spring(50, {
+    stiffness: 100,
+    damping: 26,
+  }),
+};
 
 export default class MainPage extends React.Component {
   constructor() {
@@ -8,9 +25,23 @@ export default class MainPage extends React.Component {
 
   render = () => (
     <div>
-      <h1>Main Page</h1>
-      <p>Hello World :)</p>
-      <a href="/auth/google">Sign In with Google</a>
+      <div>
+        <h1>Main Page</h1>
+        <p>Hello World :)</p>
+        <a href="/auth/google">Sign In with Google</a>
+      </div>
+      <Motion defaultStyle={fadeAnimationStart} style={fadeAnimationEnd}>
+        {value => <Image
+          circular
+          src="/assets/CandleNote-Main-Logo-Square.png"
+          style={{
+            width: '30vw',
+            opacity: value.x,
+            position: 'relative',
+            top: `${value.height}px`,
+          }}
+        />}
+      </Motion>
     </div>
   );
 }
