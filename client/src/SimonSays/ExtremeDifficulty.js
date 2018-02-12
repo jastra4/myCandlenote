ea700e8028f9230f6ad12d729f579ab61c3aa0a3
Jongerr <jontmichie@gmail.com>
2018-02-12 11:08:45 -0600

    Increase speed of blocks on extreme difficulty

--- client/src/SimonSays/ExtremeDifficulty.js
+++ client/src/SimonSays/ExtremeDifficulty.js
@@ -3,13 +3,13 @@ import React from 'react';
 const ExtremeDifficulty = (props) => {
   return (
     <div className="box">
-      <div id="red-hard" className={props.redBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('red') : () => { }}>
+      <div id="red-extreme" className={props.redBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('red') : () => { }}>
       </div>
-      <div id="green-hard" className={props.greenBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('green') : () => { }}>
+      <div id="green-extreme" className={props.greenBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('green') : () => { }}>
       </div>
-      <div id="blue-hard" className={props.blueBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('blue') : () => { }}>
+      <div id="blue-extreme" className={props.blueBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('blue') : () => { }}>
       </div>
-      <div id="yellow-hard" className={props.yellowBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('yellow') : () => { }}>
+      <div id="yellow-extreme" className={props.yellowBlink} onClick={props.isPlayersTurn ? () => props.makeTileBlink('yellow') : () => { }}>
       </div>
       <div id="dog-extreme" className={props.yellowBlink} onClick={props.isPlayersTurn ? () => props.makeAnimalSound('dog') : () => { }}>
         <img src="/assets/dog.png" style={{ width: '7em', height: '7em' }}/>
