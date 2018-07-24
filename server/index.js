aa00e1a7344944b623b9ed92c7222e35c797eef6
Joseph Strandmo <josephstrandmo@gmail.com>
2018-07-24 13:54:29 -0500

    Remove additional packages, comment out server routes, and change PORT for elastic beanstalk attampt

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const Promise = require('promise');
 const mongoose = require('mongoose');
 
 const keys = require('./config/keys');
-// const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer'); // disabled for test deployment
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -76,7 +76,8 @@ const { parseMeaningWithGoogleAPI, makePDF, getCalendarFreeBusy,
 // const SRC_DIR = path.join(__dirname,  "../client/src/");
 const DIST_DIR = path.join(__dirname, '../client/dist');
 
-const PORT = process.env.USER === 'ubuntu' ? 8080 : 3000;
+const PORT = process.env.PORT || 3000;
+// const PORT = process.env.USER === 'ubuntu' ? 8080 : 3000;
 const DOMAIN = process.env.USER === 'ubuntu' ? 'candlenote.io' : `localhost:${PORT}`;
 
 console.log('domain: ', DOMAIN);
@@ -467,219 +468,219 @@ app.post('/setReadReciept', (req, res) => {
 /* ----------- Google Cal Routes ------------ */
 
 app.post('/api/refreshToken', (req, res) => {
-  console.log('USER ID REFRESHTOKEN:', req.body.userId);
-  const { userId } = req.body;
-  queries.getRefreshToken(userId)
-    .then((data) => {
-      const { googleRefreshToken } = data;
-      return axios({
-        url: 'https://www.googleapis.com/oauth2/v4/token',
-        method: 'post',
-        params: {
-          client_id: keys.google.clientID,
-          client_secret: keys.google.clientSecret,
-          refresh_token: googleRefreshToken,
-          grant_type: 'refresh_token',
-        },
-      })
-        .then((response) => {
-          inserts.saveAccessToken({
-            userId,
-            token: response.data.access_token,
-          });
-          res.send(response.data.access_token);
-        });
-    })
-    .catch(err => console.log(err));
+  // console.log('USER ID REFRESHTOKEN:', req.body.userId);
+  // const { userId } = req.body;
+  // queries.getRefreshToken(userId)
+  //   .then((data) => {
+  //     const { googleRefreshToken } = data;
+  //     return axios({
+  //       url: 'https://www.googleapis.com/oauth2/v4/token',
+  //       method: 'post',
+  //       params: {
+  //         client_id: keys.google.clientID,
+  //         client_secret: keys.google.clientSecret,
+  //         refresh_token: googleRefreshToken,
+  //         grant_type: 'refresh_token',
+  //       },
+  //     })
+  //       .then((response) => {
+  //         inserts.saveAccessToken({
+  //           userId,
+  //           token: response.data.access_token,
+  //         });
+  //         res.send(response.data.access_token);
+  //       });
+  //   })
+  //   .catch(err => console.log(err));
 });
 
 app.post('/api/freeBusy', (req, res) => {
-  const nowInt = Date.now();
-  const nextWeekInt = nowInt + 2629746000;
-  const nowString = new Date(nowInt).toISOString();
-  const nextWeekString = new Date(nextWeekInt).toISOString();
+  // const nowInt = Date.now();
+  // const nextWeekInt = nowInt + 2629746000;
+  // const nowString = new Date(nowInt).toISOString();
+  // const nextWeekString = new Date(nextWeekInt).toISOString();
 
-  queries.getAccessToken(req.body.userId)
-    .then((doc) => {
-      const { googleAccessToken: accessToken } = doc;
-      return getCalendarList(accessToken)
-        .then(calList => calList.items)
-        .then(calIds => getCalendarFreeBusy(nowString, nextWeekString, calIds, accessToken));
-    })
-    .then((freeBusyData) => {
-      const busyTimes = reduceFreeBusyToTimeSpans(freeBusyData);
-      res.send(busyTimes);
-    })
-    .catch((err) => {
-      res.status(400).send(err);
-    });
+  // queries.getAccessToken(req.body.userId)
+  //   .then((doc) => {
+  //     const { googleAccessToken: accessToken } = doc;
+  //     return getCalendarList(accessToken)
+  //       .then(calList => calList.items)
+  //       .then(calIds => getCalendarFreeBusy(nowString, nextWeekString, calIds, accessToken));
+  //   })
+  //   .then((freeBusyData) => {
+  //     const busyTimes = reduceFreeBusyToTimeSpans(freeBusyData);
+  //     res.send(busyTimes);
+  //   })
+  //   .catch((err) => {
+  //     res.status(400).send(err);
+  //   });
 });
 
 app.post('/api/setCalendarEvents', (req, res) => {
-  const { newEvent, userIds, timeZone } = req.body;
-  const event = buildGoogleCalEvent(newEvent, timeZone);
-  Promise.all(refreshMultipleTokens(userIds))
-    .then(() => queries.getGetAccessTokensForUsers(userIds))
-    .then((results) => {
-      const accessTokens = results.map(result => result.googleAccessToken);
-      return Promise.all(setCalendarEventPerUser(accessTokens, event))
-        .then(responses => res.send(responses));
-    })
-    .catch(err => res.status(400).send(err));
+  // const { newEvent, userIds, timeZone } = req.body;
+  // const event = buildGoogleCalEvent(newEvent, timeZone);
+  // Promise.all(refreshMultipleTokens(userIds))
+  //   .then(() => queries.getGetAccessTokensForUsers(userIds))
+  //   .then((results) => {
+  //     const accessTokens = results.map(result => result.googleAccessToken);
+  //     return Promise.all(setCalendarEventPerUser(accessTokens, event))
+  //       .then(responses => res.send(responses));
+  //   })
+  //   .catch(err => res.status(400).send(err));
 });
 
 /* ----------- API Routes ------------ */
 
 app.post('/api/emailPDF', (req, res) => {
-  const { email } = req.body;
-  const filePath = path.join(__dirname, '../PDFs/70f744e6-26c4-4f7d-b0b2-c6aeebf02f0e.pdf');
-  transporter.sendMail(emailNoteOptions(email, filePath), (error) => {
-    if (error) {
-      console.error(error);
-      res.status(500).end();
-    } else {
-      console.log(`PDF successfully emailed to ${email}!`);
-      res.status(201).end();
-    }
-  });
+  // const { email } = req.body;
+  // const filePath = path.join(__dirname, '../PDFs/70f744e6-26c4-4f7d-b0b2-c6aeebf02f0e.pdf');
+  // transporter.sendMail(emailNoteOptions(email, filePath), (error) => {
+  //   if (error) {
+  //     console.error(error);
+  //     res.status(500).end();
+  //   } else {
+  //     console.log(`PDF successfully emailed to ${email}!`);
+  //     res.status(201).end();
+  //   }
+  // });
 });
 
 app.post('/api/decks', (req, res) => {
-  inserts.insertDeck(req.body)
-    .then((result) => {
-      const { _id: id, subject, title, userId } = result._doc;
-      res.send({
-        id,
-        subject,
-        title,
-        userId,
-      });
-    })
-    .catch(err => console.error(err));
+  // inserts.insertDeck(req.body)
+  //   .then((result) => {
+  //     const { _id: id, subject, title, userId } = result._doc;
+  //     res.send({
+  //       id,
+  //       subject,
+  //       title,
+  //       userId,
+  //     });
+  //   })
+  //   .catch(err => console.error(err));
 });
 
 app.post('/api/deleteDeck', (req, res) => {
-  deletes.deleteDeck(req.body.deckId)
-    .then(() => {
-      const { _id: id } = req.body;
-      res.send(id);
-    })
-    .catch(err => console.error(err));
+  // deletes.deleteDeck(req.body.deckId)
+  //   .then(() => {
+  //     const { _id: id } = req.body;
+  //     res.send(id);
+  //   })
+  //   .catch(err => console.error(err));
 });
 
 app.post('/api/flashcards', (req, res) => {
-  inserts.insertFlashcard(req.body)
-    .then((result) => {
-      const { _id: id, front, back, deckId, userId } = result._doc;
-      res.send({
-        id,
-        front,
-        back,
-        deckId,
-        userId,
-      });
-    })
-    .catch(err => console.error(err));
+  // inserts.insertFlashcard(req.body)
+  //   .then((result) => {
+  //     const { _id: id, front, back, deckId, userId } = result._doc;
+  //     res.send({
+  //       id,
+  //       front,
+  //       back,
+  //       deckId,
+  //       userId,
+  //     });
+  //   })
+  //   .catch(err => console.error(err));
 });
 
 app.post('/api/deleteCard', (req, res) => {
-  deletes.deleteFlashcard(req.body)
-    .then(() => res.send('Deleted'))
-    .catch(err => console.error(err));
+  // deletes.deleteFlashcard(req.body)
+  //   .then(() => res.send('Deleted'))
+  //   .catch(err => console.error(err));
 });
 
 
 app.get('/api/getNotes/:id', (req, res) => {
-  const { id: userId } = req.params;
-  queries.getNotes(userId)
-    .then((notes) => { res.send(notes); })
-    .catch((e) => {
-      console.error(e);
-      res.sendStatus(500).end();
-    });
+  // const { id: userId } = req.params;
+  // queries.getNotes(userId)
+  //   .then((notes) => { res.send(notes); })
+  //   .catch((e) => {
+  //     console.error(e);
+  //     res.sendStatus(500).end();
+  //   });
 });
 
 app.post('/api/deleteNote/', (req, res) => {
-  const { noteId } = req.body;
-  deletes.deleteNote(noteId);
-  res.end();
+  // const { noteId } = req.body;
+  // deletes.deleteNote(noteId);
+  // res.end();
 });
 
 app.post('/api/parseContentMeaning', (req, res) => {
-  parseMeaningWithGoogleAPI(req.body.content)
-    .then((meaning) => {
-      res.send({ meaning });
-    })
-    .catch(((e) => {
-      console.error(e);
-      res.status(500).end();
-    }));
+  // parseMeaningWithGoogleAPI(req.body.content)
+  //   .then((meaning) => {
+  //     res.send({ meaning });
+  //   })
+  //   .catch(((e) => {
+  //     console.error(e);
+  //     res.status(500).end();
+  //   }));
 });
 
 
 app.post('/api/suggestedResources', (req, res) => {
-  axios.get('https://www.googleapis.com/customsearch/v1', { params: {
-    q: req.body.searchTerms,
-    key: process.env.GOOGLE_SEARCH_API_KEY,
-    cx: process.env.GOOGLE_SEARCH_API_ID,
-  } })
-    .then((results) => { res.send(results.data); })
-    .catch((err) => { // eslint-disable-line
-      console.error('⚠️  Error searching Google');
-      res.sendStatus(500).end();
-    });
+  // axios.get('https://www.googleapis.com/customsearch/v1', { params: {
+  //   q: req.body.searchTerms,
+  //   key: process.env.GOOGLE_SEARCH_API_KEY,
+  //   cx: process.env.GOOGLE_SEARCH_API_ID,
+  // } })
+  //   .then((results) => { res.send(results.data); })
+  //   .catch((err) => { // eslint-disable-line
+  //     console.error('⚠️  Error searching Google');
+  //     res.sendStatus(500).end();
+  //   });
 });
 
 app.post('/api/suggestedVideos', (req, res) => {
-  axios.get('https://www.googleapis.com/youtube/v3/search', { params: {
-    maxResults: 5,
-    part: 'snippet',
-    q: req.body.searchTerms,
-    type: 'video',
-    key: process.env.YOUTUBE_DATA_API_KEY,
-    videoEmbeddable: 'true',
-  } })
-    .then(result => res.send(result.data))
-    .catch((err) => { // eslint-disable-line
-      console.error('⚠️  Error searching YouTube');
-      res.sendStatus(500).end();
-    });
+  // axios.get('https://www.googleapis.com/youtube/v3/search', { params: {
+  //   maxResults: 5,
+  //   part: 'snippet',
+  //   q: req.body.searchTerms,
+  //   type: 'video',
+  //   key: process.env.YOUTUBE_DATA_API_KEY,
+  //   videoEmbeddable: 'true',
+  // } })
+  //   .then(result => res.send(result.data))
+  //   .catch((err) => { // eslint-disable-line
+  //     console.error('⚠️  Error searching YouTube');
+  //     res.sendStatus(500).end();
+  //   });
 });
 
 app.post('/api/suggestedWiki', (req, res) => {
-  const searchTerms = req.body.searchTerms.split(' ').slice(0, 3).join('+');
-  axios.get('https://en.wikipedia.org/w/api.php', { params: {
-    action: 'opensearch',
-    search: req.body.searchTerms,
-    limit: 10,
-    namespace: 0,
-    format: 'json',
-  } })
-    .then((result) => {
-      // If no results
-      if (!result.data[1].length) {
-        axios.get('https://www.googleapis.com/customsearch/v1', { params: {
-          q: `${searchTerms}+wikipedia`,
-          key: process.env.GOOGLE_SEARCH_API_KEY,
-          cx: process.env.GOOGLE_WIKI_SEARCH_KEY,
-        } })
-          .then((googleResult) => {
-            const { data } = googleResult;
-            data.isFromGoogle = true;
-            res.send(data);
-          })
-          .catch((err) => { // eslint-disable-line
-            console.error('⚠️  Error searching Google');
-            res.sendStatus(500).end();
-          });
-      } else {
-        res.send(result.data);
-      }
-    })
-    .catch((err) => { // eslint-disable-line
-      console.error('⚠️  Error searching Wikipedia');
-      res.sendStatus(500).end();
-    });
+  // const searchTerms = req.body.searchTerms.split(' ').slice(0, 3).join('+');
+  // axios.get('https://en.wikipedia.org/w/api.php', { params: {
+  //   action: 'opensearch',
+  //   search: req.body.searchTerms,
+  //   limit: 10,
+  //   namespace: 0,
+  //   format: 'json',
+  // } })
+  //   .then((result) => {
+  //     // If no results
+  //     if (!result.data[1].length) {
+  //       axios.get('https://www.googleapis.com/customsearch/v1', { params: {
+  //         q: `${searchTerms}+wikipedia`,
+  //         key: process.env.GOOGLE_SEARCH_API_KEY,
+  //         cx: process.env.GOOGLE_WIKI_SEARCH_KEY,
+  //       } })
+  //         .then((googleResult) => {
+  //           const { data } = googleResult;
+  //           data.isFromGoogle = true;
+  //           res.send(data);
+  //         })
+  //         .catch((err) => { // eslint-disable-line
+  //           console.error('⚠️  Error searching Google');
+  //           res.sendStatus(500).end();
+  //         });
+  //     } else {
+  //       res.send(result.data);
+  //     }
+  //   })
+  //   .catch((err) => { // eslint-disable-line
+  //     console.error('⚠️  Error searching Wikipedia');
+  //     res.sendStatus(500).end();
+  //   });
 });
 
 
@@ -787,116 +788,116 @@ app.post('/api/generatePDF', (req, res) => {
 
 
 app.post('/api/getEditorPacket', (req, res) => {
-  const { noteToPrint } = req.body;
-  queries.getPacket(noteToPrint)
-    .then((result) => {
-      const { body: packet, authorID, title, showDate, showTitle, showName } = result[0];
-      queries.getUsernameById(authorID)
-        .then(({ username }) => {
-          res.send({
-            packet, title, username, showDate, showTitle, showName,
-          });
-        });
-    })
-    .catch((e) => {
-      console.error(e);
-      res.sendStatus(500).end();
-    });
+  // const { noteToPrint } = req.body;
+  // queries.getPacket(noteToPrint)
+  //   .then((result) => {
+  //     const { body: packet, authorID, title, showDate, showTitle, showName } = result[0];
+  //     queries.getUsernameById(authorID)
+  //       .then(({ username }) => {
+  //         res.send({
+  //           packet, title, username, showDate, showTitle, showName,
+  //         });
+  //       });
+  //   })
+  //   .catch((e) => {
+  //     console.error(e);
+  //     res.sendStatus(500).end();
+  //   });
 });
 
 app.post('/api/userDecks', (req, res) => {
-  const { userId } = req.body;
-  queries.getDecksForUser(userId)
-    .then((response) => {
-      const decks = response.map((deck) => {
-        const { _id: id, subject, title, userId: uid } = deck;
-        return {
-          id,
-          subject,
-          title,
-          userId: uid,
-        };
-      });
-      res.send(decks);
-    })
-    .catch(err => res.send(err));
+  // const { userId } = req.body;
+  // queries.getDecksForUser(userId)
+  //   .then((response) => {
+  //     const decks = response.map((deck) => {
+  //       const { _id: id, subject, title, userId: uid } = deck;
+  //       return {
+  //         id,
+  //         subject,
+  //         title,
+  //         userId: uid,
+  //       };
+  //     });
+  //     res.send(decks);
+  //   })
+  //   .catch(err => res.send(err));
 });
 
 app.post('/api/userFlashcards', (req, res) => {
-  const { userId } = req.body;
-  queries.getFlashcardsForUser(userId)
-    .then((response) => {
-      const flashcards = response.map((card) => {
-        const { _id: id, front, back, deckId, userId: uid } = card;
-        return {
-          id,
-          front,
-          back,
-          deckId,
-          userId: uid,
-        };
-      });
-      res.send(flashcards);
-    })
-    .catch(err => res.send(err));
+  // const { userId } = req.body;
+  // queries.getFlashcardsForUser(userId)
+  //   .then((response) => {
+  //     const flashcards = response.map((card) => {
+  //       const { _id: id, front, back, deckId, userId: uid } = card;
+  //       return {
+  //         id,
+  //         front,
+  //         back,
+  //         deckId,
+  //         userId: uid,
+  //       };
+  //     });
+  //     res.send(flashcards);
+  //   })
+  //   .catch(err => res.send(err));
 });
 
 
 app.post('/api/userFriends', (req, res) => {
-  const { userId } = req.body;
-  queries.getCurrentUser(userId)
-    .then(response => response.friends.map(friend => friend.friendId))
-    .then(friendIds => queries.getFriendsById(friendIds))
-    .then((friendsInfo) => {
-      const friends = friendsInfo.map((friend) => {
-        const { _id: id, username, profileImage } = friend;
-        return {
-          id,
-          username,
-          profileImage,
-        };
-      });
-      res.send(friends);
-    })
-    .catch(err => res.status(400).send(err));
+  // const { userId } = req.body;
+  // queries.getCurrentUser(userId)
+  //   .then(response => response.friends.map(friend => friend.friendId))
+  //   .then(friendIds => queries.getFriendsById(friendIds))
+  //   .then((friendsInfo) => {
+  //     const friends = friendsInfo.map((friend) => {
+  //       const { _id: id, username, profileImage } = friend;
+  //       return {
+  //         id,
+  //         username,
+  //         profileImage,
+  //       };
+  //     });
+  //     res.send(friends);
+  //   })
+  //   .catch(err => res.status(400).send(err));
 });
 
 app.post('/api/addFriend', (req, res) => {
-  const { userId, friendId } = req.body;
-  inserts.addFriend(userId, friendId)
-    .then(response => res.send(response))
-    .catch(err => res.status(400).send(err));
+  // const { userId, friendId } = req.body;
+  // inserts.addFriend(userId, friendId)
+  //   .then(response => res.send(response))
+  //   .catch(err => res.status(400).send(err));
 });
 
 app.post('/api/removeFriend', (req, res) => {
-  const { userId, friendId } = req.body;
-  deletes.removeFriendById(userId, friendId)
-    .then(results => res.send(results))
-    .catch((err) => {
-      console.log('Error removing friend:', err);
-      res.sendStatus(400);
-    });
+  // const { userId, friendId } = req.body;
+  // deletes.removeFriendById(userId, friendId)
+  //   .then(results => res.send(results))
+  //   .catch((err) => {
+  //     console.log('Error removing friend:', err);
+  //     res.sendStatus(400);
+  //   });
 });
 
 app.post('/api/userByUsername', (req, res) => {
-  const { username } = req.body;
-  queries.getUserByUsername(username)
-    .then(user => res.send(user))
-    .catch(err => res.status(400).send(err));
+  // const { username } = req.body;
+  // queries.getUserByUsername(username)
+  //   .then(user => res.send(user))
+  //   .catch(err => res.status(400).send(err));
 });
 
 app.post('/api/userById', (req, res) => {
-  const { userId } = req.body;
-  queries.getCurrentUser(userId)
-    .then((user) => {
-      const { _id: id, username, profileImage } = user;
-      res.send({
-        id,
-        username,
-        profileImage,
-      });
-    })
-    .catch(err => res.status(400).send(err));
+  // const { userId } = req.body;
+  // queries.getCurrentUser(userId)
+  //   .then((user) => {
+  //     const { _id: id, username, profileImage } = user;
+  //     res.send({
+  //       id,
+  //       username,
+  //       profileImage,
+  //     });
+  //   })
+  //   .catch(err => res.status(400).send(err));
 });
 
 app.get('*', (req, res) => {
